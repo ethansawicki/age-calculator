0b64e3ea8661ab2eae7356f15c1055569feea5cf
Ethan Sawicki <ethansawicki2797@gmail.com>
2022-08-02 00:50:56 -0500

    adding js practice files

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+const birthYear = 1990
+const futureYear = 2077
+const fakeAge = birthYear + futureYear
+const realAge = futureYear - birthYear
+
+console.log(`I will be either ${fakeAge} or ${realAge} in ${futureYear}`)
